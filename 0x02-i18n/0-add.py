bf6606628b6f55da4a4d870a82afbaa9b1e7b17a
GirumZ <girum.zebe@gmail.com>
2024-05-28 12:03:33 +0300

    [Create]: simple flask app and index file

--- 0x02-i18n/0-add.py
+++ 0x02-i18n/0-add.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', method=[GET], strict_slashes=False)
 def index():
     """Index route"""
     return render_template('0-index.html')
